11c018c8c36bfc2994591175864c4f641a211f32
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2019-11-05 15:14:25 +0100

    initial commit

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="header">
+    <h1>TodoList</h1>
+    <div id="app">
+      <div class="nav">
+        <router-link class="nav-item" to="/">Home</router-link>|
+        <router-link to="/about">About</router-link>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+  background: #333;
+  text-align: center;
+  padding: 10px;
+}
+
+.header a {
+  color: #fff;
+  padding-right: 5px;
+  text-decoration: none;
+}
+
+h1 {
+  color: #fff;
+}
+
+.nav {
+  color: rgb(181, 32, 201);
+}
+</style>
